8e4ec51d71af85f0b0e52a9ca4c8c1f84fdbecbe
Serigne Saliou Gueye <gueyesaliou067@gmail.com>
2019-11-05 05:11:16 +0100

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mouride-authentique-style'));
+app.use(express.static(__dirname + '/dist/chikory'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/mouride-authentique-style/index.html'));
+res.sendFile(path.join(__dirname+'/dist/chikory/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
